2123426ba90c9bcc48ceb848d7e00f46e2127954
DarkMechanikum <v.shtarev08@gmail.com>
2024-03-28 15:51:29 +0200

    testVER1

--- pull_and_run.sh
+++ pull_and_run.sh
@@ -1,3 +1,2 @@
-docker rmi -f darkmechanikum/robotic_arm_jass2024
-docker build -t darkmechanikum/robotic_arm_jass2024 . 
+docker pull darkmechanikum/robotic_arm_jass2024
 docker run --privileged --mount type=bind,source=./workdir,target=/kinova/workdir -it darkmechanikum/robotic_arm_jass2024
