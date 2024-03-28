3bcf827a0bf9823ae39b3f8cfb910fcb03ec02ef
DarkMechanikum <v.shtarev08@gmail.com>
2024-03-28 16:02:13 +0200

    ;

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+docker rm -f robotarm
 docker rmi -f darkmechanikum/robotic_arm_jass2024
 docker build -t darkmechanikum/robotic_arm_jass2024 . 
-docker run --privileged --mount type=bind,source=./workdir,target=/kinova/workdir -it darkmechanikum/robotic_arm_jass2024
+docker run --privileged --mount type=bind,source=./workdir,target=/kinova/workdir --name robotarm -it darkmechanikum/robotic_arm_jass2024
